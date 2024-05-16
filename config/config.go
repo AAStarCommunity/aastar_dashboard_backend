69c69e1fc8af4c26108565129f55d83c1cecb037
徐超越 <993921@qq.com>
2024-05-17 00:09:27 +0800

    github oauth support

--- config/config.go
+++ config/config.go
@@ -3,14 +3,20 @@ package config
 import "C"
 import (
 	"aastar_dashboard_back/model"
+	"aastar_dashboard_back/rpc_server/controller/oauth"
 	"fmt"
-	"github.com/spf13/viper"
 	"os"
+
+	"github.com/spf13/viper"
 )
 
 var dscTemplate = "host=%s port=%s user=%s password=%s dbname=%s TimeZone=%s sslmode=%s"
 var Environment *model.Env
-var SystemConfigViper *viper.Viper
+var systemConfigViper *viper.Viper
+
+func AllConfig() map[string]any {
+	return systemConfigViper.AllSettings()
+}
 
 func Init(configPath string) {
 	envName := model.DevEnv
@@ -23,21 +29,25 @@ func Init(configPath string) {
 			return envName != model.ProdEnv
 		}(),
 	}
-	SystemConfigViper = viper.New()
-	SystemConfigViper.SetConfigFile(configPath)
-	err := SystemConfigViper.ReadInConfig()
+	systemConfigViper = viper.New()
+	systemConfigViper.SetConfigFile(configPath)
+	err := systemConfigViper.ReadInConfig()
 	if err != nil {
 		panic(err)
 	}
+
+	githubClientId := systemConfigViper.GetString("OAuth.Github.ClientId")
+	githubClientSecret := systemConfigViper.GetString("OAuth.Github.ClientSecret")
+	oauth.SetGithubOAuthAppInfo(&githubClientId, &githubClientSecret)
 }
 func GetDsn() string {
 	return fmt.Sprintf(dscTemplate,
-		SystemConfigViper.GetString("DB.host"),
-		SystemConfigViper.GetString("DB.port"),
-		SystemConfigViper.GetString("DB.user"),
-		SystemConfigViper.GetString("DB.password"),
-		SystemConfigViper.GetString("DB.db_name"),
-		SystemConfigViper.GetString("DB.tz"),
-		SystemConfigViper.GetString("DB.ssl_mode"),
+		systemConfigViper.GetString("DB.host"),
+		systemConfigViper.GetString("DB.port"),
+		systemConfigViper.GetString("DB.user"),
+		systemConfigViper.GetString("DB.password"),
+		systemConfigViper.GetString("DB.db_name"),
+		systemConfigViper.GetString("DB.tz"),
+		systemConfigViper.GetString("DB.ssl_mode"),
 	)
 }
---
+++ rpc_server/controller/oauth/github.go
@@ -0,0 +1,166 @@
+package oauth
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	clientId     string
+	clientSecret string
+)
+
+func SetGithubOAuthAppInfo(clientIdPtr, clientSecretPrt *string) {
+	clientId = *clientIdPtr
+	clientSecret = *clientSecretPrt
+}
+
+type githubAccessToken struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	Scope       string `json:"scope"`
+}
+
+/*
+	{
+	  "login": "fanhousanbu",
+	  "id": 6702542,
+	  "node_id": "MDQ6VXNlcjY3MDI1NDI=",
+	  "avatar_url": "https://avatars.githubusercontent.com/u/6702542?v=4",
+	  "gravatar_id": "",
+	  "url": "https://api.github.com/users/fanhousanbu",
+	  "html_url": "https://github.com/fanhousanbu",
+	  "followers_url": "https://api.github.com/users/fanhousanbu/followers",
+	  "following_url": "https://api.github.com/users/fanhousanbu/following{/other_user}",
+	  "gists_url": "https://api.github.com/users/fanhousanbu/gists{/gist_id}",
+	  "starred_url": "https://api.github.com/users/fanhousanbu/starred{/owner}{/repo}",
+	  "subscriptions_url": "https://api.github.com/users/fanhousanbu/subscriptions",
+	  "organizations_url": "https://api.github.com/users/fanhousanbu/orgs",
+	  "repos_url": "https://api.github.com/users/fanhousanbu/repos",
+	  "events_url": "https://api.github.com/users/fanhousanbu/events{/privacy}",
+	  "received_events_url": "https://api.github.com/users/fanhousanbu/received_events",
+	  "type": "User",
+	  "site_admin": false,
+	  "name": "徐超越",
+	  "company": null,
+	  "blog": "https://www.gotocloud.vip",
+	  "location": "杭州",
+	  "email": "go-to-cloud@qq.com",
+	  "hireable": null,
+	  "bio": null,
+	  "twitter_username": null,
+	  "public_repos": 17,
+	  "public_gists": 0,
+	  "followers": 10,
+	  "following": 4,
+	  "created_at": "2014-02-17T08:06:15Z",
+	  "updated_at": "2024-05-10T05:15:11Z"
+	}
+*/
+type githubUserInfo struct {
+	Id        int    `json:"id"`
+	Login     string `json:"login"`
+	AvatarUrl string `json:"avatar_url"`
+	Url       string `json:"url"`
+	Name      string `json:"name"`
+}
+
+func getGithubAccessToken(token string) (*githubAccessToken, error) {
+	req, err := http.NewRequest(
+		"POST",
+		"https://github.com/login/oauth/access_token?"+
+			"client_id="+clientId+"&"+
+			"client_secret="+clientSecret+"&"+
+			"code="+token,
+		nil)
+	if err != nil {
+		fmt.Println("GithubAccessToken Request failed:", err)
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println("Post Failed:", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		fmt.Println("StatusCode:", resp.StatusCode)
+		return nil, err
+	}
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("ReadAll error:", err)
+		return nil, err
+	}
+
+	ret := &githubAccessToken{}
+	items := strings.Split(string(responseBody), "&")
+	accessToken := strings.Split(items[0], "=")[1]
+	scope := strings.Split(string(items[1]), "=")[1]
+	tokenType := strings.Split(string(items[2]), "=")[1]
+	ret.AccessToken = accessToken
+	ret.Scope = scope
+	ret.TokenType = tokenType
+	return ret, err
+}
+
+func getGithubUserInfo(accessToken *githubAccessToken) (*githubUserInfo, error) {
+	req, err := http.NewRequest(
+		"GET",
+		"https://api.github.com/user",
+		nil)
+	if err != nil {
+		fmt.Println("GithubAccessToken Request failed:", err)
+		return nil, err
+	}
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Authorization", accessToken.TokenType+" "+accessToken.AccessToken)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println("Get Failed:", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		fmt.Println("StatusCode:", resp.StatusCode)
+		return nil, err
+	}
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("ReadAll error:", err)
+		return nil, err
+	}
+
+	githubUserInfo := &githubUserInfo{}
+	err = json.Unmarshal(responseBody, githubUserInfo)
+	return githubUserInfo, err
+}
+
+func GithubOAuthLogin(ctx *gin.Context) {
+	token := ctx.Query("code")
+
+	if accessToken, err := getGithubAccessToken(token); err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+		return
+	} else {
+		if githubUser, err := getGithubUserInfo(accessToken); err != nil {
+			ctx.JSON(http.StatusBadRequest, err)
+			return
+		} else {
+			// TODO: save / update user info to db
+			_ = githubUser
+		}
+	}
+}
